2904132bfaa507f9e0a21ca77ee219077ba6a81e
kwon.yuchan <kwon.yuchan@nbplus.kr>
2018-06-06 14:53:27 +0900

    get_title

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 from urllib.request import urlopen
+from urllib.error import HTTPError
 from bs4 import BeautifulSoup
 
-html = urlopen('http://pythonscraping.com/pages/page1.html')
-bs = BeautifulSoup(html.read(), 'html.parser')
-print(bs.h1)
+
+def get_title(url):
+    try:
+        html = urlopen(url)
+    except HTTPError:
+        return None
+    try:
+        bs = BeautifulSoup(html.read(), 'html.parser')
+        v = bs.body.h1
+    except AttributeError:
+        return None
+    return v
+
+
+title = get_title('http://www.pythonscraping.com/pages/page1.html')
+if title is None:
+    print('Title could not be found')
+else:
+    print(title)
