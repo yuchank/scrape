082f6189e8cfec51bd075db49687ff77583d690b
kwon.yuchan <kwon.yuchan@nbplus.kr>
2018-06-11 01:07:53 +0900

    find

--- 02css.py
+++ 02css.py
@@ -7,3 +7,10 @@ bs = BeautifulSoup(html.read(), 'html.parser')
 nameList = bs.findAll('span', {'class': 'green'})
 for name in nameList:
     print(name.get_text())
+
+bs.find_all(['h1', 'h2', 'h3', 'h4', 'h5', 'h6'])
+bs.find_all('span', {'class': {'green', 'red'}})
+nameList = bs.find_all(text='the prince')
+print(len(nameList))
+
+title = bs.find_all(id='title', class_='text')
